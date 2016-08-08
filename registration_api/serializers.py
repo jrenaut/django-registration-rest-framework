54324efc6d92c2ae71620f9c32015cd9db3bcfc1
Jon Renaut <jrenaut@gmail.com>
2016-08-07 22:23:35 -0400

    maybe it's not so crazy to remove the password from the serializer

--- registration_api/serializers.py
+++ registration_api/serializers.py
@@ -5,3 +5,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+
+    def to_native(self, obj):
+        """Remove password field when serializing an object"""
+        ret = super(UserSerializer, self).to_native(obj)
+        del ret['password']
+        return ret
--- registration_api/views.py
+++ registration_api/views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-#from django.views.decorators.csrf import csrf_exempt
 
 import utils
 from serializers import UserSerializer
